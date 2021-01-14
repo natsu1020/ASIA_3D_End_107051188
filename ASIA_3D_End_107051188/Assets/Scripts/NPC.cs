5f8be3cdb4fe9dc7af52ad803a2bf5e02d84eb62
natsu1020 <71679925+natsu1020@users.noreply.github.com>
2021-01-15 07:54:02 +0800

    1/15

---
+++ ASIA_3D_End_107051188/Assets/Scripts/NPC.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC 資料")]
+    public NPCData data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+    [Header("對話者名稱")]
+    public Text textName;
+    [Header("對話間隔")]
+    public float interval = 0.2f;
+
+
+
+    ///<summary>
+    /// 是否進入感應區
+    /// </summary>
+    public bool playerInArea;
+
+    public enum NPCState
+    {
+        FirstDialog,Missioning,Finish
+    }
+
+    public NPCState state = NPCState.FirstDialog;
+
+
+    
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "少女")
+        {
+            playerInArea = true;
+           StartCoroutine( Dialog());
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "少女")
+        {
+            playerInArea = false;
+            StopDialog();
+        }
+    }
+
+    private void StopDialog()
+    {
+        dialog.SetActive(false);
+        StopAllCoroutines();
+    }
+
+
+    private IEnumerator Dialog()
+    {
+        dialog.SetActive(true);
+
+        textContent.text = "";
+
+        textName.text = name;
+
+        for (int i = 0; i< data.dialougA.Length; i++)
+        {
+            textContent.text += data.dialougA[i] + "";
+            yield return new WaitForSeconds(interval);
+        }
+
+    }
+
+
+}
---
+++ ASIA_3D_End_107051188/Assets/Scripts/NPCData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+// ScriptableObject腳本化物件
+
+[CreateAssetMenu(fileName ="NPC 資料", menuName ="Natsu/NPC 資料")]
+public class NPCData : ScriptableObject
+
+{
+    [Header("第一段對話"), TextArea(1, 5)]
+    public string dialougA;
+    [Header("第一段對話"), TextArea(1, 5)]
+    public string dialougB;
+    [Header("第一段對話"), TextArea(1, 5)]
+    public string dialougC;
+
+
+}
